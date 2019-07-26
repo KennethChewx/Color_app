686506d223c37af7ec80e8132ae65ca719990957
Kenneth Chew <kennethchew_11@otmail.com>
2019-07-26 12:14:03 +0800

    remove checkpoint

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -157,7 +157,6 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  discriminator=discriminator)
 #Restore weights
 #checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-checkpoint.restore(checkpoint_dir)
 
 def allowed_file(filename):
     return '.' in filename and \
