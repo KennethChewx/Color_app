f5af9ed7f9063e4fee786f1a66721ed7ce190d90
Kenneth Chew <kennethchew_11@otmail.com>
2019-07-30 16:03:37 +0800

    Update main

--- main.py
+++ main.py
@@ -197,8 +197,6 @@ def upload_file():
             image = tf.expand_dims(image,0)            
             prediction = generator(image, training=True)
             prediction = prediction[0] * 0.5 + 0.5
-            plt.figure(figsize=(15,15))
-            plt.subplot(1,1,1)
             plt.imshow(prediction)
             plt.axis('off')
             plt.savefig('static/colored/'+ str(filename), bbox_inches = 'tight', pad_inches = 0)
